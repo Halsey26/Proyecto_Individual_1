bbbd75869087cf45497760c86e09b28b88387d6c
Halsey26 <omara.cassano@gmail.com>
2024-09-17 04:16:20 -0500

    ultimas modificaciones

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
+import pandas as pd
 
 app = FastAPI()
+
+#lectura del archivo movie
+movie_api = pd.read_parquet('../datasets/movie_dataset_final.parquet', engine= 'pyarrow')
+
+    # X cantidad de películas fueron estrenadas en el mes de 
+def f_filmaciones_mes(df, mes, column):
+    mes= mes.capitalize()
+    pelis_mes= df[df[column]==mes ]   
+
+    len_films = len(pelis_mes)
+    return f"{len_films} cantidad de películas fueron estrenadas en el mes de {mes}"
 
 
 @app.get("/inicio")
 async def ruta_prueba():
     return "Hola"
+
+
+@app.get("/Cantidad_Filmacione_Mes/{mes}")
+def cantidad_filmaciones_mes(mes:str):
+    return {"message":f_filmaciones_mes(movie_api,mes,'release_meses')}
